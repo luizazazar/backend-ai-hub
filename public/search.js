d66a2713c23a28d4d39fd4d5beed23f888d44a19
luizazazar <169006837+luizazazar@users.noreply.github.com>
2024-05-26 15:25:04 +0000

    Updated DB for Search Bar

--- public/search.js
+++ public/search.js
@@ -2,9 +2,9 @@
 const items = [
     { title: 'AIBots', description: "Build your chatbot in 15 minutes", link: "./aibotsproductpage", about: "RAG, retrieval augmented generation, chatbots, design chatbot, knowledge management" },
     { title: 'Pair Chat', description: 'Fast and secure version of ChatGPT.', link: "./pairchatproductpage", about: "chatgpt-equivalent, ideation, brainstorming, language translation, coding, rewriting or editing text, review text, spelling, grammar" },
-    { title: 'Transcribe', description: 'Record and summarise your meeting minutes.', link: "./transcribeproductpage", about: "diarisation, transcription, interviews, speech to text" },
+    { title: 'Transcribe', description: 'Record and summarise your meeting minutes.', link: "./transcribeproductpage", about: "diarisation, transcription, interviews, speech to text, summarise minutes, summarise meeting minutes, summarise interview minutes" },
     { title: 'LaunchPad', description: 'Share your ideas.' , link: "./launchpadproductpage", about: "innovate, ideathon, hackathon" },
-    { title: 'ReadLiao', description: 'Simplify complicated letters for seniors.', link: "./readliaoproductpage", about: "translate, OCR, speech to text" },
+    { title: 'ReadLiao', description: 'Simplify complicated letters for seniors.', link: "./readliaoproductpage", about: "translate, OCR, speech to text, scan letters" },
     { title: 'SmartCompose', description: 'Draft customer service replies in 5 minutes.', link: "./smartcomposeproductpage", about: "reply customer emails, data redaction, draft reply" }
     // Add more items as needed
 ];
