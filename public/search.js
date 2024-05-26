09b1947362198b34b06ec7c62e0b1ceebd45877d
luizazazar <169006837+luizazazar@users.noreply.github.com>
2024-05-26 16:02:28 +0000

    Updated DB with SmartCompose keywords

--- public/search.js
+++ public/search.js
@@ -4,8 +4,8 @@ const items = [
     { title: 'Pair Chat', description: 'Fast and secure version of ChatGPT.', link: "./pairchatproductpage", about: "chatgpt-equivalent, ideation, brainstorming, language translation, coding, rewriting or editing text, review text, spelling, grammar" },
     { title: 'Transcribe', description: 'Record and summarise your meeting minutes.', link: "./transcribeproductpage", about: "speech to text, diarisation, transcription, interviews, speech to text, summarise minutes, audio transcription, summarise meeting minutes, summarise interview minutes" },
     { title: 'LaunchPad', description: 'Share your ideas.' , link: "./launchpadproductpage", about: "innovate, ideathon, hackathon" },
-    { title: 'ReadLiao', description: 'Simplify complicated letters for seniors.', link: "./readliaoproductpage", about: "translate, OCR, speech to text, scan letters, translate english to mandarin, scan" },
-    { title: 'SmartCompose', description: 'Draft customer service replies in 5 minutes.', link: "./smartcomposeproductpage", about: "reply emails, reply customer emails, data redaction, draft reply, service reply" }
+    { title: 'ReadLiao', description: 'Simplify complicated letters for seniors.', link: "./readliaoproductpage", about: "translate, OCR, speech to text, scan letters, translate english to mandarin, scan, elderly" },
+    { title: 'SmartCompose', description: 'Draft customer service replies in 5 minutes.', link: "./smartcomposeproductpage", about: "reply emails, reply customer emails, data redaction, draft reply, service reply, compose reply" }
     // Add more items as needed
 ];
 
