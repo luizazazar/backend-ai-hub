e61fb117fca2950bfe8519cec79b553a60fe21eb
luizazazar <169006837+luizazazar@users.noreply.github.com>
2024-05-26 12:18:50 +0000

    Updated links in search bar results

--- public/search.js
+++ public/search.js
@@ -1,11 +1,11 @@
 // In-memory list of items (variable)
 const items = [
-    { title: 'AIBots', description: "Create your chatbot in 15 minutes", link: "./AIBots Page.html", about: "ChatGPT is useful but it only contains internet data. When you can include a knowledge base (until Restricted\Sensitive-Normal), there are many more possibilities where you can leverage large language models to help improve your agency's work and productivity. AIBots is a platform allowing agencies to create AI Chatbots with pre-configured system prompts and an added knowledge base (.pdf, .docx, .txt files)." },
-    { title: 'Pair Chat', description: 'Fast and secure version of ChatGPT.' },
-    { title: 'Transcribe', description: 'Record and summarise your meeting minutes.' },
-    { title: 'LaunchPad', description: 'Share your ideas.' },
-    { title: 'ReadLiao', description: 'Simplify complicated letters for seniors.' },
-    { title: 'SmartCompose', description: 'Draft customer service replies in 5 minutes.' }
+    { title: 'AIBots', description: "Create your chatbot in 15 minutes", link: "./aibotsproductpage", about: "ChatGPT is useful but it only contains internet data. When you can include a knowledge base (until Restricted\Sensitive-Normal), there are many more possibilities where you can leverage large language models to help improve your agency's work and productivity. AIBots is a platform allowing agencies to create AI Chatbots with pre-configured system prompts and an added knowledge base (.pdf, .docx, .txt files)." },
+    { title: 'Pair Chat', description: 'Fast and secure version of ChatGPT.', link: "./pairchatproductpage" },
+    { title: 'Transcribe', description: 'Record and summarise your meeting minutes.', link: "./transcribeproductpage" },
+    { title: 'LaunchPad', description: 'Share your ideas.' , link: "./launchpadproductpage" },
+    { title: 'ReadLiao', description: 'Simplify complicated letters for seniors.', link: "./readliaoproductpage" },
+    { title: 'SmartCompose', description: 'Draft customer service replies in 5 minutes.', link: "./smartcomposeproductpage" }
     // Add more items as needed
 ];
 
