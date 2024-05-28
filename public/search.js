be3894b44ce9cf19988a1c220f06d3f4c1c6b7d1
luizazazar <169006837+luizazazar@users.noreply.github.com>
2024-05-28 15:30:55 +0000

    Minor Text Edits to Appraiser and Search DB.

--- public/search.js
+++ public/search.js
@@ -1,13 +1,13 @@
 // In-memory list of items (variable)
 const items = [
-    { title: 'AIBots', description: "Build your chatbot in 15 minutes", link: "./aibotsproductpage", about: "RAG, retrieval augmented generation, chatbots, design chatbot, knowledge management, info overload" },
-    { title: 'Pair Chat', description: 'Fast and secure version of ChatGPT.', link: "./pairchatproductpage", about: "chatgpt-equivalent, ideation, brainstorming, language translation, coding, rewriting or editing text, review text, spelling, grammar" },
-    { title: 'Transcribe', description: 'Record and summarise your meeting minutes.', link: "./transcribeproductpage", about: "speech to text, diarisation, transcription, interviews, speech to text, summarise minutes, audio transcription, summarise meeting minutes, summarise interview minutes" },
-    { title: 'LaunchPad', description: 'Share your ideas.' , link: "./launchpadproductpage", about: "innovate, ideathon, hackathon" },
+    { title: 'AIBots', description: "Build your Chatbot in 15 minutes", link: "./aibotsproductpage", about: "RAG, retrieval augmented generation, chatbots, design chatbot, knowledge management, info overload" },
+    { title: 'Pair Chat', description: 'Fast and Secure version of ChatGPT.', link: "./pairchatproductpage", about: "chatgpt-equivalent, ideation, brainstorming, language translation, coding, rewriting or editing text, review text, spelling, grammar" },
+    { title: 'Transcribe', description: 'Record and Summarise your Meeting Minutes.', link: "./transcribeproductpage", about: "speech to text, diarisation, transcription, interviews, speech to text, summarise minutes, audio transcription, summarise meeting minutes, summarise interview minutes" },
+    { title: 'LaunchPad', description: 'Share your Ideas.' , link: "./launchpadproductpage", about: "innovate, ideathon, hackathon" },
     { title: 'ReadLiao', description: 'Simplify and Translate Complicated Letters.', link: "./readliaoproductpage", about: "translate, OCR, speech to text, scan, scan letters, scan documents, translate english to mandarin, scan, elderly" },
-    { title: 'SmartCompose', description: 'Draft customer service replies in 5 minutes.', link: "./smartcomposeproductpage", about: "reply emails, reply customer emails, data redaction, draft reply, service reply, compose reply" },
+    { title: 'SmartCompose', description: 'Draft Customer Service Replies in 5 minutes.', link: "./smartcomposeproductpage", about: "reply emails, reply customer emails, data redaction, draft reply, service reply, compose reply" },
     { title: 'AISAY', description: 'An AI Document Reader.', link: "./aisayproductpage", about: "scan, OCR, scan documents, scan letters, passports, scan invoice" },
-    { title: 'Appraiser', description: 'A testimonial generator', link: "./appraiserproductpage", about: "draft, draft testimonial, students, student reports, recommend" },
+    { title: 'Appraiser', description: 'A Testimonial Generator', link: "./appraiserproductpage", about: "draft, draft testimonial, students, student reports, recommend" },
     { title: 'SearchSG', description: 'A WOG Search Engine.', link: "./searchsgproductpage", about: "search engine, search analytics, improve discoverability, discoverability" }
     // Add more items as needed
 ];
